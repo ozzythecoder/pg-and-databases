9ca7cefa2034d5e0188dbc8703edae0f2ca078f0
August McAllister <august.chm@gmail.com>
2022-11-08 15:39:55 -0600

    Added pool support

--- server/routes/songs.router.js
+++ server/routes/songs.router.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+  database: 'songs', // name of database
+  host: 'localhost', // database server
+  port: 5432, // Postgres default
+  max: 10, // max queries at once
+  idleTimeoutMillis: 30000 // 30 seconds to try to connect before cancelling query
+});
+
+// not required, but useful for debugging:
+pool.on('connect', () => {
+  console.log('postgresql is connected! shout out node');
+});
+
+pool.on('error', (error) => {
+  console.log('epic fail in postgres pool.', error);
+})
+
 let songs = [
     {
         rank: 355, 
